0ce9b01108c9e4e3dc9f13e62b5e7a6e1a0cb595
dzhelek <yoan.dzhelekarski@gmail.com>
2023-04-07 11:35:04 +0300

    rebrand grafana

--- public/app/core/components/Branding/Branding.tsx
+++ public/app/core/components/Branding/Branding.tsx
@@ -59,8 +59,8 @@ export class Branding {
   static LoginBackground = LoginBackground;
   static MenuLogo = MenuLogo;
   static LoginBoxBackground = LoginBoxBackground;
-  static AppTitle = 'Grafana';
-  static LoginTitle = 'Welcome to Grafana';
+  static AppTitle = 'STEM';
+  static LoginTitle = 'Welcome to STEM';
   static GetLoginSubTitle = (): null | string => {
     return null;
   };
